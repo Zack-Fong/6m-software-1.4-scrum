e651bbe32ef72c99afd236c0b8e4c93df44a3dd8
Fong Zi Hao <PACE.K2320388H@assoc.main.ntu.edu.sg>
2023-02-27 21:57:27 +0800

    1st Attempt on Assignment

--- assignment/q1-smart-home.js
+++ assignment/q1-smart-home.js
@@ -7,23 +7,40 @@
 */
 
 // Task: Add code here
+class BaseSignal {
+  #type;
+  constructor(type) {
+    if (this.constructor == BaseSignal) {
+      throw new Error("This class cannot be instantiated");
+    }
+
+    this.#type = type;
+  }
+
+  send() {
+    console.log(`Sending ${this.#type} signal`);
+  }
+}
 
 class TvSignal extends BaseSignal {
-    constructor(){
-        // Add code here
-    }
+  constructor() {
+    // Add code here
+    super("tv");
+  }
 }
 
 class AirconSignal extends BaseSignal {
-    constructor(){
-        // Add code here
-    }
+  constructor() {
+    // Add code here
+    super("aircon");
+  }
 }
 
 class DoorSignal extends BaseSignal {
-    constructor(){
-        // Add code here
-    }
+  constructor() {
+    // Add code here
+    super("door");
+  }
 }
 
 const tv = new TvSignal();
